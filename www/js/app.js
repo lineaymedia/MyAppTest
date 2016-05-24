3194acf8d27c60e167dffdc8dc9052278b369799
santiago777 <santiagofloresoyola@gmail.com>
2016-05-24 17:12:00 +0200

    boton menu
    nuevo boton de menu

--- www/js/app.js
+++ www/js/app.js
@@ -67,6 +67,15 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         controller: 'PlaylistCtrl'
       }
     }
+
+    .state('app.santiago', {
+    url: '/santiago',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/santiago.html'
+      }
+    
+    }
   });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/playlists');
